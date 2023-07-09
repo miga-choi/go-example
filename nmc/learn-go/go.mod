698687506c9f8dd51c56da2d4a3e7a0bb18f9fb6
miga-choi <miga.choi.1004@gmail.com>
2023-07-09 21:53:45 +0900

    nmc: try constants & variables

---
+++ nmc/learn-go/go.mod
@@ -0,0 +1,3 @@
+module nmc/learn-go
+
+go 1.19
--- nmc/learn-go/main.go
+++ nmc/learn-go/main.go
@@ -6,4 +6,21 @@ import (
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	// constants
+	const const1 = "const1"
+	fmt.Println(const1)
+	const const2 string = "const2"
+	fmt.Println(const2)
+
+	// variables
+	var1 := "var1"
+	fmt.Println(var1)
+	var1 = "changedVar1"
+	fmt.Println(var1)
+
+	var var2 string = "var2"
+	fmt.Println(var2)
+	var2 = "changedVar2"
+	fmt.Println(var2)
 }
