26147e3db5f57bf709d9d88f0215a74bfe5c5b1d
miga-choi <miga.choi.1004@gmail.com>
2023-07-10 19:50:05 +0900

    nmc: try function

--- nmc/learn-go/main.go
+++ nmc/learn-go/main.go
@@ -2,7 +2,24 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
+
+func plus(a int, b int) int {
+	return a + b
+}
+
+func multiply(a, b int) int {
+	return a * b
+}
+
+func lenAndUpper(name string) (int, string) {
+	return len(name), strings.ToUpper(name)
+}
+
+func repeatMe(words ...string) {
+	fmt.Println(words)
+}
 
 func main() {
 	fmt.Println("Hello, World!")
@@ -23,4 +40,19 @@ func main() {
 	fmt.Println(var2)
 	var2 = "changedVar2"
 	fmt.Println(var2)
+
+	// functions
+	fmt.Println(plus(2, 2))
+	fmt.Println(multiply(2, 2))
+
+	// function returns 2 value
+	totalLength1, upperName1 := lenAndUpper("ian")
+	fmt.Println(totalLength1, upperName1)
+
+	// "_" will ignore returned value
+	totalLength2, _ := lenAndUpper("choi")
+	fmt.Println(totalLength2)
+
+	// function with multiple arguments
+	repeatMe("hello", "world", "ian", "choi")
 }
