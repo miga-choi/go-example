96964325e284f9ec4e7eff4129afa9f134590485
miga-choi <miga.choi.1004@gmail.com>
2023-07-14 22:53:13 +0900

    nmc: try "Pointer receivers" and "Error handling"

--- nmc/bank_and_dictionary/accounts/accounts.go
+++ nmc/bank_and_dictionary/accounts/accounts.go
@@ -1,5 +1,7 @@
 package accounts
 
+import "errors"
+
 // Account struct
 type Account struct {
 	// upper case => public
@@ -9,9 +11,36 @@ type Account struct {
 	balance int
 }
 
+var errInsufficientBalance = errors.New("Insufficient balance!")
+
 // NewAccount creates Account
 func NewAccount(owner_ string) *Account {
 	// can omit balance
 	account := Account{Owner: owner_} // balance => 0
 	return &account
+}
+
+// Deposit amount on your account
+// account_ without "*", account_ is a copy of parameter, so origin account_ won't increase
+func (account_ Account) WrongDeposit(amount_ int) {
+	account_.balance += amount_
+}
+
+// account_ with "*", account_ point to origin account_, so origin account_ will increase
+func (account_ *Account) Deposit(amount_ int) {
+	account_.balance += amount_
+}
+
+// Withdraw amount from your account
+func (account_ *Account) Withdraw(amount_ int) error {
+	if account_.balance < amount_ {
+		return errInsufficientBalance
+	}
+	account_.balance -= amount_
+	return nil
+}
+
+// Balance of your account
+func (account_ Account) Balance() int {
+	return account_.balance
 }
--- nmc/bank_and_dictionary/main.go
+++ nmc/bank_and_dictionary/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"nmc/bank_and_dictionary/accounts"
 )
 
@@ -14,4 +15,24 @@ func main() {
 
 	newAccount.Owner = "hi"
 	fmt.Println(newAccount)
+
+	account.WrongDeposit(100)
+	fmt.Println(account.Balance())
+
+	account.Deposit(100)
+	fmt.Println(account.Balance())
+
+	err := account.Withdraw(10)
+	if err != nil {
+		log.Fatalln(err)
+	} else {
+		fmt.Println(account.Balance())
+	}
+
+	err = account.Withdraw(100)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(account.Balance())
+	}
 }
